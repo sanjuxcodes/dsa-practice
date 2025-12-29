33250d9f692010cf76727793eb96ae914f3e0567
sanjuxcodes <sd9735122@gmail.com>
2025-12-29 16:00:07 +0530

    finding insertion point into a sorted array

---
+++ Arrays/insertionpoint.java
@@ -0,0 +1,26 @@
+public class insertionpoint{
+    static int found(int arr[],int t){
+
+        int n= arr.length;
+        int ans =n;
+        int low =0;
+        int high=n-1;
+        while(low<=high){
+            int mid= (low+high)/2;
+            
+            if(arr[mid]>=t){
+                ans=mid;
+                high=mid-1;
+
+            }
+
+            else
+                low=mid+1;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,3,3,3,4,8,9};
+        System.out.print("ele should be inserted into indx: "+found(arr, 21));
+    }
+}
