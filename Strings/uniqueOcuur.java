17635b3989ddbfc8fa5f309c21e07be911433352
sanjuxcodes <sd9735122@gmail.com>
2026-02-25 14:17:27 +0530

    findig frist unique element present in string

---
+++ Strings/uniqueOcuur.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] freq= new int[26];
+
+        for(int i =0;i<s.length();i++){
+            char ch=s.charAt(i);
+            freq[ch-'a']++;//count and reg indx into freq array....
+        }
+
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(freq[ch-'a']==1){//chchk 
+                return i;
+            }
+        }
+
+        return -1;
+        
+    }
+}
