0e3f654467dfd0bab8a2ff6720e09d7a73152d6f
sanjuxcodes <sd9735122@gmail.com>
2026-04-16 10:05:35 +0530

    time pass

---
+++ Arrays/stars.java
@@ -0,0 +1,15 @@
+public class stars {
+    public static void main(String[] args) {
+        for(int i=6;i>0;i--){
+            System.out.println();
+            for (int j = i; j >0 ;j--){
+                System.out.print("q");
+            }
+              
+            
+        }
+    }
+}
+
+
+
