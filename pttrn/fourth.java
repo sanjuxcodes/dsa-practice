e66dafdf5ffc9e01f6d8bb393dd1afbd3a3d389e
sanjuxcodes <sd9735122@gmail.com>
2026-06-07 09:36:15 +0530

    tcsx

---
+++ pttrn/fourth.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class fourth{
+
+
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int t=sc.nextInt();
+
+        while(t>0){
+
+            int r = sc.nextInt();
+
+            for(int i=r;i>0;i--){
+
+                for(int j=0;j<i;j++){
+
+                    System.out.print(j+1);
+
+                }
+
+                System.out.println();
+
+
+            }
+
+            
+
+
+
+
+
+            t--;
+        }
+    }
+    
+}
