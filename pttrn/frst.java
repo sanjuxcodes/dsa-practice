f4f11c875011aff1f98d483d2c4c10c20834ae25
sanjuxcodes <sd9735122@gmail.com>
2026-06-07 09:20:13 +0530

    tcsx

---
+++ pttrn/frst.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class frst {
+
+
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int t=sc.nextInt();
+
+        while(t>0){
+            int r= sc.nextInt();
+            
+            for(int i =r;i>=0;i--){
+                for(int j=0;j<=i;j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
+            
+            t--;
+        }
+    }
+    
+}
