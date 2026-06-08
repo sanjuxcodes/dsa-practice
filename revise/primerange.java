7bbfbf3e3b7ad06a099307cde7f79aa8c2264282
sanjuxcodes <sd9735122@gmail.com>
2026-06-08 10:24:00 +0530

    tcsx

---
+++ revise/primerange.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+public class primerange {
+
+    static boolean isprime(int n){
+        if(n<2){
+            return false;
+        }
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                return false;
+                
+            }
+            
+        }
+        return true;
+    }
+    static void pr(int a,int b){
+
+        for(int i=a;i<=b;i++){
+            if(isprime(i)){
+                System.out.println(i);
+            }
+        }
+
+    }
+    static void sum(int a , int b){
+
+        int sum=0;
+
+        for(int i=a;i<=b;i++){
+            if(isprime(i)){
+                sum+=i;
+            }
+        }
+        System.out.print(sum);
+
+
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int f=sc.nextInt();
+        int l = sc.nextInt();
+
+        sum(f,l);
+
+
+
+    }
+
+
+    
+}
