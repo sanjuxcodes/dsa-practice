327be5df0abecdb9661bb0f26d97b2927257bc2c
sanjuxcodes <sd9735122@gmail.com>
2026-06-15 11:39:41 +0530

    tcsx

---
+++ revise/shop.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class shop {
+    public static void main(String[] a){
+
+        Scanner sc = new Scanner(System.in);
+
+        int i=sc.nextInt();
+
+        int b=sc.nextInt();
+
+        int fs=(int)Math.pow(2,b);
+
+        System.out.print(fs-i);
+
+
+    }
+    
+}
