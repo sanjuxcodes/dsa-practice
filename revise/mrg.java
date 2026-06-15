98c46903202ecde74bec62de58275a662e76ba7b
sanjuxcodes <sd9735122@gmail.com>
2026-06-15 12:06:55 +0530

    tcsx

---
+++ revise/mrg.java
@@ -0,0 +1,66 @@
+import java.util.*;
+public class mrg {
+
+    public static void main(String[] ar){
+
+        Scanner sc = new Scanner(System.in);
+
+        int a= sc.nextInt();
+        int arr[]= new int[a];
+
+        for(int l=0;l<a;l++){
+            arr[l]=sc.nextInt();
+
+        }
+        
+
+        int b = sc.nextInt();
+
+        int brr[]= new int[b];
+
+        for(int m=0;m<b;m++){
+            brr[m]=sc.nextInt();
+        }
+
+
+
+        int i=0;
+        int j=0;
+        ArrayList<Integer> map= new ArrayList<>();
+
+
+        while(i<a && j<b){
+
+            if(arr[i]<=brr[j]){
+
+                map.add(arr[i]);
+                i++;
+
+
+            }
+            else{
+                map.add(brr[j]);
+                j++;
+            }
+            if(i==a){
+
+                while(j<b){
+                    map.add(brr[j]);
+                    j++;
+                }
+
+            }
+            if(j==b){
+                while(i<a){
+                    map.add(arr[i]);
+                    i++;
+                }
+
+            }
+
+        }
+        System.out.print(map);
+
+    }
+    
+}
