7cef2696e8ada5858cd62210d04e678465d966bb
sanjuxcodes <sd9735122@gmail.com>
2026-06-21 10:30:22 +0530

    tcs

---
+++ revise/doc.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class doc {
+
+    public static void main(String[] ar){
+
+        Scanner sc = new Scanner(System.in);
+
+        int p=sc.nextInt();
+
+        int arv= sc.nextInt();
+
+        
+
+        int doc=0;
+        int pa=0;
+
+
+
+        int wt=0;
+
+
+        for(int i=0;i<p;i++){
+            
+            wt=doc-pa;
+         
+            pa+=arv;
+            doc+=10;
+
+           
+           
+
+        }
+
+       System.out.print(wt);
+        
+
+    }
+    
+}
