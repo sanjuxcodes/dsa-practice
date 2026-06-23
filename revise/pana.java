435edc679eadf2ba099f056e3cc66c1ccf0d13e3
sanjuxcodes <sd9735122@gmail.com>
2026-06-23 10:45:23 +0530

    tcsx

---
+++ revise/pana.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class pana {
+
+    public static void main(String[] ar){
+
+        Scanner sc = new Scanner(System.in);
+
+        String ip = sc.nextLine().toLowerCase();
+        String jp=ip.replaceAll("[^a-zA-Z] ","");
+    
+
+        int arr[]= new int[26];
+
+        for(char ch : jp.toCharArray()){
+            arr[ch-'a']++;
+
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                System.out.print("Not panagram");
+                return;
+
+            }
+        }
+        System.out.print("Panagram");
+
+    }
+    
+}
