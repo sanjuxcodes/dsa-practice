8a1ae18fc74e83253856bfdf0a3aa857594dea53
sanjuxcodes <sd9735122@gmail.com>
2026-06-24 08:51:30 +0530

    tcs

---
+++ revise/movez.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class movez {
+
+    public static void main(String[] ar){
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+
+      
+
+        int j=0;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+
+            if(arr[i] !=0){
+                arr[j]=arr[i];
+                j++;
+            }
+
+
+        }
+        for(int i=j+1;i<n;i++){
+
+            arr[i]=0;
+
+
+        }
+        for(int i: arr){
+            System.out.print(i+" ");
+        }
+
+
+    }
+    
+}
