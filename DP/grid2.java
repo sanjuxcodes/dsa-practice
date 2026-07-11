56d211c5cd33657292ba263287f0c4e1bb8e6a6d
sanjuxcodes <sd9735122@gmail.com>
2026-07-11 08:55:59 +0530

    dps

---
+++ DP/grid2.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+
+        int mod = (int)(1e9+7);
+
+        if(obstacleGrid[0][0] ==1){
+            return 0;
+        }
+
+        int dp[][]= new int[obstacleGrid.length][obstacleGrid[0].length];
+
+   
+
+        for(int i =0;i<obstacleGrid.length;i++){
+            for(int j=0;j<obstacleGrid[0].length;j++){
+
+                if(i==0 &&j==0){
+                    dp[i][j]=1;
+                }
+
+                else if((i>0 ||j>0) && obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                }
+
+                else{
+                    int up=0;
+                    int left=0;
+
+                    if(i>0){
+                        up=dp[i-1][j];
+                    }
+                    if(j>0){
+                        left=dp[i][j-1];
+                    }
+
+                    dp[i][j]=up+left;
+                }
+
+
+
+            }
+        }
+
+        return dp[obstacleGrid.length-1][obstacleGrid[0].length-1] ;
+        
+    }
+}
