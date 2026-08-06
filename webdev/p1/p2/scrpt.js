10beddebc84673e18ef8be9dfa16fbaf1ee227b4
sanjuxcodes <sd9735122@gmail.com>
2026-08-06 15:55:13 +0530

    building for her

---
+++ webdev/p1/p2/scrpt.js
@@ -0,0 +1,20 @@
+
+
+alert("Love u")
+confirm("do u ??")
+
+var ans=prompt("Enter ur ans :")
+
+let nm =prompt("ur name : ")
+
+function greet(s){
+    console.log("mrng ",s);
+}
+function res(a){
+    console.log("her ans is: ",a)
+}
+greet(nm)
+
+res(ans)
+
+
