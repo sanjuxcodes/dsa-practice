566922ddc880ec6ef5c8f370a946748a111151b5
sanjuxcodes <sd9735122@gmail.com>
2026-08-07 22:58:49 +0530

    dom done

--- webdev/p1/p2/scrpt.js
+++ webdev/p1/p2/scrpt.js
@@ -1,6 +1,6 @@
 
 
-alert("Love u")
+/* alert("Love u")
 confirm("do u ??")
 
 var ans=prompt("Enter ur ans :")
@@ -17,4 +17,61 @@ greet(nm)
 
 res(ans)
 
+var user={
+    unm:"sanju",
+    age:22,
+    grt:function(){
+        console.log("good mrng")
+
+    } 
+}
+
+user.grt() */
+
+
+
+var box=document.querySelector(".box")
+let l=0
+
+box.addEventListener("click",function(){
+
+    if(l==0){
+
+    box.innerHTML="Goruuuu",
+    box.style.color="lightgreen"
+    box.style.backgroundColor="red"
+    l=1
+    }
+    else{
+
+        box.innerHTML="Anyways I love u "
+        box.style.color="black"
+
+        box.style.backgroundColor="lightpink"
+        l=0
+    }
+
+
+
+
+})
+
+let btn=document.querySelector("#btn")
+var blb=document.querySelector("#light")
+
+var f=0
+
+btn.addEventListener("click",function(){
+    if(f==0){
+    blb.style.backgroundColor="yellow"
+    f=1
+    }
+    else{
+         blb.style.backgroundColor="white"
+        
+        f=0
+
+    }
+
+})
 
