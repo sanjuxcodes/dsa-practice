047ed8fb19913196ba2a66d549b22ab5e62bb197
sanjuxcodes <sd9735122@gmail.com>
2026-08-08 12:59:37 +0530

    union all

--- dbs/uni.sql
+++ dbs/uni.sql
@@ -1,4 +1,4 @@
--- union 
+-- union (only fetch unique valeus)
 -- when have 2 or more select or condition with same data use union
 -- lc 1789
 -- # Write your MySQL query statement below
@@ -8,3 +8,13 @@ union
 select employee_id , department_id from Employee where primary_flag='Y';
 
 
+-- lc 602
+-- union all(fetch all values with duplicates as well)
+-- here used union as CTE then fetched required info from cte
+
+select requester_id as id, count(*) as num from 
+(select requester_id from RequestAccepted 
+union all
+select accepter_id from RequestAccepted)t 
+ group by id 
+ order by num desc limit 1;
