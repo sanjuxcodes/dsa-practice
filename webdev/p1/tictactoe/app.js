6383954794dd4916c452499bfe578fc4ed0abb8e
sanjuxcodes <sd9735122@gmail.com>
2026-08-25 21:34:19 +0530

    Front ENd

---
+++ webdev/p1/tictactoe/app.js
@@ -0,0 +1,52 @@
+let box= document.querySelectorAll(".btn");
+
+let rest= document.querySelector(".reset");
+
+var turn =true;
+
+let winningPatterns = [
+    [0, 1, 2],  // top row
+    [3, 4, 5],  // middle row
+    [6, 7, 8],  // bottom row
+
+    [0, 3, 6],  // left columns
+    [1, 4, 7],  // middle column
+    [2, 5, 8],  // right column
+
+    [0, 4, 8],  // diagonal
+    [2, 4, 6]   // diagonal
+];
+
+box.forEach((btn)=>{
+    btn.addEventListener("click",()=>{
+
+
+        if(turn){
+     
+            btn.innerText='X';
+
+            turn=false;
+        }else{
+
+            btn.innerText='O';
+            turn =true;
+
+        }
+
+
+        btn.disabled=true;
+
+
+        
+    })
+
+    checkwinner();
+
+})
+
+
+
+let checkwinner = ()=>{
+    
+
+}
